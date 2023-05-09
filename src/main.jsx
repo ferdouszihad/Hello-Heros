cc0a6d98cdcdf663b045b7a85d5f6249603f265e
ferdouszihad <f.zihad333@gmail.com>
2023-05-09 21:30:47 +0600

    Router Set up completed & Layout Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <h2>This is Default Route</h2>,
+      },
+      {
+        path: "login",
+        element: <h2>This is Login Route</h2>,
+      },
+      {
+        path: "register",
+        element: <h2>This is Register Route</h2>,
+      },
+      {
+        path: "instructors/all",
+        element: <h2>All Instructors are Here</h2>,
+      },
+      {
+        path: "instructors/:id",
+        element: <h2>Single Instructors are Here</h2>,
+      },
+      {
+        path: "instructors/add",
+        element: <h2>Instructors Can be Added From Here</h2>,
+      },
+      {
+        path: "instructors/update/:id",
+        element: <h2>Instructors information can be Updated from There</h2>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
